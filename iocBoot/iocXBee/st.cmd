e330ac1a438b8eb34b428a9ee59f314e62a31476
mrolland <mrolland@debian.bnl.gov>
2019-02-22 11:55:46 -0500

    Cleanup

--- iocBoot/iocXBee/st.cmd
+++ iocBoot/iocXBee/st.cmd
@@ -1,12 +1,5 @@
 #!../../bin/linux-x86_64/XBee
 
-## Location of stream protocol files
-epicsEnvSet("ENGINEER",  "kgofron x5283")
-epicsEnvSet("LOCATION", "740 BM6")
-
-#epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
-#epicsEnvSet("EPICS_CA_ADDR_LIST", "10.6.0.255")
-
 < envPaths
 
 epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/protocol")
@@ -15,7 +8,7 @@ epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/protocol")
 dbLoadDatabase "$(TOP)/dbd/XBee.dbd"
 XBee_registerRecordDeviceDriver pdbbase
 
-# Configure serial port for Rex-F9000 controller
+# Configure IP port for gateway
 drvAsynIPPortConfigure("XBEE","10.10.192.18:12000")
 
 ## Load record instances
